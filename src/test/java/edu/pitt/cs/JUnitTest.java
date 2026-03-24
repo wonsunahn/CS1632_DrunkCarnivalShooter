bcb372cdce75af2f6569e2be6cce92600bf6d172
wonsunahn <54322243+wonsunahn@users.noreply.github.com>
2026-03-24 08:00:09 -0400

    Added a JUnit test and modified pom.xml configuration to generate report even on a test failure.

---
+++ src/test/java/edu/pitt/cs/JUnitTest.java
@@ -0,0 +1,13 @@
+package edu.pitt.cs;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JUnitTest {
+    @Test
+    public void testGetRoundString() {
+        DrunkCarnivalShooter shooter = DrunkCarnivalShooter.createInstance(InstanceType.IMPL);
+        assertEquals("Round 1: || || || || ", shooter.getRoundString());
+    }
+}
